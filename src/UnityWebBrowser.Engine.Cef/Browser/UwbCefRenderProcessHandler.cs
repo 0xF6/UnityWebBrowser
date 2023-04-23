a28306d795649fd9d2663fb4af22c89eb662adb6
Yuuki Wesp <ls-micro@ya.ru>
2023-04-23 12:14:39 +0300

    bump

--- src/UnityWebBrowser.Engine.Cef/Browser/UwbCefRenderProcessHandler.cs
+++ src/UnityWebBrowser.Engine.Cef/Browser/UwbCefRenderProcessHandler.cs
@@ -3,6 +3,7 @@
 // 
 // This project is under the MIT license. See the LICENSE.md file for more details.
 
+using System.CommandLine;
 using Xilium.CefGlue;
 
 namespace UnityWebBrowser.Engine.Cef.Browser;
@@ -21,5 +22,17 @@ public class UwbCefRenderProcessHandler : CefRenderProcessHandler
 
         CefV8Value cefVersion = CefV8Value.CreateString(CefRuntime.ChromeVersion);
         v8Object.SetValue("webEngineVersion", cefVersion);
+        
+        CefV8Value dotnet_execute = CefV8Value.CreateFunction("dotnet_execute", new Test());
+
+        v8Object.SetValue("dotnet_execute", dotnet_execute);
+    }
+}
+
+public class Test : CefV8Handler
+{
+    protected override bool Execute(string name, CefV8Value obj, CefV8Value[] arguments, out CefV8Value returnValue, out string exception)
+    {
+        throw new System.NotImplementedException();
     }
 }
